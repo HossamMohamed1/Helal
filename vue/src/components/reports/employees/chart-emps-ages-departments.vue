a511cb76616c58031f583b33baf6f93eedf6d68a
Eslam ISmail <eslan.ismail93@gmail.com>
2023-01-09 17:47:33 +0200

    FA

--- vue/src/components/reports/employees/chart-emps-ages-departments.vue
+++ vue/src/components/reports/employees/chart-emps-ages-departments.vue
@@ -10,97 +10,106 @@
 </template>
 
 <script>
+import { mapActions } from "vuex";
+
 export default {
   data() {
     return {
       showChart: false,
-      series: [{
-        name: "قسم 1",
-        data: [45, 52, 38, 24, 33, 26, 21, 20, 44, 45, 21, 20, 44, 45,30, 35, 37, 38, 53, 55, 33, 26]
-      },
-        {
-          name: "قسم 2",
-          data: [35, 41, 62, 42, 13, 38, 24, 33, 26, 21, 46, 13, 18, 29, 37, 24, 18, 29, 37, 24, 45]
-        },
-        {
-          name: "قسم 3",
-          data: [35, 51, 62, 42, 44, 45,30, 35, 37, 38, 42, 16, 18, 29, 37, 24, 45, 52,24, 18, 29, 67]
-        },
-        {
-          name: "قسم 4",
-          data: [35, 41, 52, 42, 23, 38, 24, 33, 26, 21, 32, 17, 18, 29, 37, 24, 45, 52, 18, 29, 37, 24]
-        }
-      ],
+      series: [],
       chartOptions: {
         chart: {
           height: 350,
-          type: 'line',
+          type: "line",
           zoom: {
-            enabled: false
+            enabled: false,
           },
         },
         dataLabels: {
-          enabled: false
+          enabled: false,
         },
         stroke: {
           width: [2, 2, 2, 2, 2],
           // curve: 'straight',
-          curve: 'smooth',
+          curve: "smooth",
           // dashArray: [0, 8, 5]
         },
         title: {
-          text: '',
-          align: 'center'
+          text: "",
+          align: "center",
         },
         legend: {
-          tooltipHoverFormatter: function(val, opts) {
-            return val + ' - ' + opts.w.globals.series[opts.seriesIndex][opts.dataPointIndex] + ''
-          }
+          tooltipHoverFormatter: function (val, opts) {
+            return (
+              val +
+              " - " +
+              opts.w.globals.series[opts.seriesIndex][opts.dataPointIndex] +
+              ""
+            );
+          },
         },
         markers: {
           size: 0,
           hover: {
-            sizeOffset: 6
-          }
+            sizeOffset: 6,
+          },
         },
         xaxis: {
-          categories: [20,21, 22, 23,24, 25,26, 27, 28, 29, 30, 30, 35, 40,41, 42, 43, 44, 45, 50, 55, 60
-          ],
+          categories: [],
         },
-        colors: ['#1e8e49', '#fd9162', '#a7a7a7','#41b27b',  '#c9de77'],
+        colors: ["#1e8e49", "#fd9162", "#a7a7a7", "#41b27b", "#c9de77"],
         tooltip: {
           y: [
             {
               title: {
                 formatter: function (val) {
-                  return val + " موظف "
-                }
-              }
+                  return val + " موظف ";
+                },
+              },
             },
             {
               title: {
                 formatter: function (val) {
-                  return val + " موظف "
-                }
-              }
+                  return val + " موظف ";
+                },
+              },
             },
             {
               title: {
                 formatter: function (val) {
                   return val;
-                }
-              }
-            }
-          ]
+                },
+              },
+            },
+          ],
         },
         grid: {
-          borderColor: '#f1f1f1',
-        }
+          borderColor: "#f1f1f1",
+        },
       },
-    }
+    };
   },
   mounted() {
-    this.showChart = true
-  }
-}
+    this.showChart = true;
+    const data = {
+      charts: ["line"],
+      type: "department_age",
+    };
+    this.fetchChart(data)
+      .then((res) => {
+        const { line } = res ?? {};
+        const { labels, result } = line ?? {};
+        this.series = result ?? [];
+        this.chartOptions.xaxis.categories = labels ?? [];
+
+        this.showChart = true;
+      })
+      .catch(() => {
+        this.showChart = true;
+      });
+  },
+  methods: {
+    ...mapActions("statistics", ["fetchChart"]),
+  },
+};
 </script>
