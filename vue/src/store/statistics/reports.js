cb5f40ac8277dc665ef4481f06a00011163952e5
Eslam ISmail <eslan.ismail93@gmail.com>
2023-01-12 00:19:06 +0200

    FA

--- vue/src/store/statistics/reports.js
+++ vue/src/store/statistics/reports.js
@@ -2,52 +2,52 @@ const reports = [
   {
     title: "employees.employeesByDepartment",
     chart: "employee_department",
-    type: ["bar", "line", "pie"],
+    type: ["bar"],
   },
   {
     title: "employees.employeesByGender",
     chart: "employee_gender",
-    type: ["bar", "line", "pie"],
+    type: ["pie"],
   },
   {
     title: "employees.employeesByHeadquarters",
     chart: "employee_location",
-    type: ["bar", "line", "pie"],
+    type: ["bar"],
   },
   {
     title: "employees.employee_nationality",
     chart: "employee_nationality",
-    type: ["bar", "line", "pie"],
+    type: ["bar"],
   },
   {
     title: "employees.employeesAccordingToTheirQualifications",
     chart: "employee_qualification",
-    type: ["bar", "line", "pie"],
+    type: ["line"],
   },
   {
     title: "employees.employeesAccordingToTheirRanks",
     chart: "employee_major",
-    type: ["bar", "line", "pie"],
+    type: ["bar"],
   },
   {
     title: "employees.employee_job",
     chart: "employee_job",
-    type: ["bar", "line", "pie"],
+    type: ["bar"],
   },
   {
     title: "employees.EmployeesAccordingAttendance",
     chart: "employee_absence",
-    type: ["bar", "line", "pie"],
+    type: ["pie"],
   },
   {
     title: "employees.department_status",
     chart: "department_status",
-    type: ["bar", "line", "pie"],
+    type: ["bar"],
   },
   {
     title: "employees.department_location",
     chart: "department_location",
-    type: ["bar", "line", "pie"],
+    type: ["bar"],
   },
 ];
 
