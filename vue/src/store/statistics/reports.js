e474560564c65808a407cf7dc6fbb98e7c70e8e9
Eslam ISmail <eslan.ismail93@gmail.com>
2023-01-15 11:48:59 +0200

    FA

--- vue/src/store/statistics/reports.js
+++ vue/src/store/statistics/reports.js
@@ -1,20 +1,20 @@
 const reports = [
-  {
-    title: "employees.employeesByDepartment",
-    chart: "employee_department",
-    type: [
-      { text: "line", value: "line" },
-      // { text: "pie", value: "pie" },
-    ],
-  },
-  {
-    title: "employees.employeesByGender",
-    chart: "employee_gender",
-    type: [
-      { text: "pie", value: "pie" },
-      { text: "donut", value: "pie" },
-    ],
-  },
+  // {
+  //   title: "employees.employeesByDepartment",
+  //   chart: "employee_department",
+  //   type: [
+  //     { text: "line", value: "line" },
+  //     // { text: "pie", value: "pie" },
+  //   ],
+  // },
+  // {
+  //   title: "employees.employeesByGender",
+  //   chart: "employee_gender",
+  //   type: [
+  //     { text: "pie", value: "pie" },
+  //     { text: "donut", value: "pie" },
+  //   ],
+  // },
   {
     title: "employees.employeesByHeadquarters",
     chart: "employee_location",
@@ -25,41 +25,41 @@ const reports = [
       { text: "donut", value: "pie" },
     ],
   },
-  {
-    title: "employees.employee_nationality",
-    chart: "employee_nationality",
-    type: [{ text: "bar", value: "bar" }],
-  },
-  {
-    title: "employees.employeesAccordingToTheirQualifications",
-    chart: "employee_qualification",
-    type: [{ text: "line", value: "line" }],
-  },
-  {
-    title: "employees.employeesAccordingToTheirRanks",
-    chart: "employee_major",
-    type: [{ text: "line", value: "line" }],
-  },
-  {
-    title: "employees.employee_job",
-    chart: "employee_job",
-    type: [{ text: "line", value: "line" }],
-  },
   // {
-  //   title: "employees.EmployeesAccordingAttendance",
-  //   chart: "employee_absence",
-  //   type: ["pie"],
+  //   title: "employees.employee_nationality",
+  //   chart: "employee_nationality",
+  //   type: [{ text: "bar", value: "bar" }],
   // },
-  {
-    title: "employees.department_status",
-    chart: "department_status",
-    type: [{ text: "bar", value: "line" }],
-  },
-  {
-    title: "employees.department_location",
-    chart: "department_location",
-    type: [{ text: "bar", value: "bar" }],
-  },
+  // {
+  //   title: "employees.employeesAccordingToTheirQualifications",
+  //   chart: "employee_qualification",
+  //   type: [{ text: "line", value: "line" }],
+  // },
+  // {
+  //   title: "employees.employeesAccordingToTheirRanks",
+  //   chart: "employee_major",
+  //   type: [{ text: "line", value: "line" }],
+  // },
+  // {
+  //   title: "employees.employee_job",
+  //   chart: "employee_job",
+  //   type: [{ text: "line", value: "line" }],
+  // },
+  // // {
+  // //   title: "employees.EmployeesAccordingAttendance",
+  // //   chart: "employee_absence",
+  // //   type: ["pie"],
+  // // },
+  // {
+  //   title: "employees.department_status",
+  //   chart: "department_status",
+  //   type: [{ text: "bar", value: "line" }],
+  // },
+  // {
+  //   title: "employees.department_location",
+  //   chart: "department_location",
+  //   type: [{ text: "bar", value: "bar" }],
+  // },
 ];
 
 export default reports;
