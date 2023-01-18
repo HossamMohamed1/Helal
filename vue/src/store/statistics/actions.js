15c89d36e9b1731b035ff45d6f4593a5f00d86ec
Eslam ISmail <eslan.ismail93@gmail.com>
2023-01-18 13:42:17 +0200

    FA

--- vue/src/store/statistics/actions.js
+++ vue/src/store/statistics/actions.js
@@ -20,7 +20,7 @@ const actions = {
       cards = state.cards.map((item) => ({
         ...item,
         loading: false,
-        value: parseFloat(data[item.backend]),
+        value: parseFloat(data[item.backend] ?? 0),
       }));
       commit("setCards", cards);
     } catch (error) {
