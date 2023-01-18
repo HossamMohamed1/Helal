30cc0b0271eac24b30b90436be57f074348530df
Wakeb <Wakeb@Alharameen.gov.sa>
2023-01-18 14:06:05 +0300

    FA

--- backend/app/Http/Controllers/ReportController.php
+++ backend/app/Http/Controllers/ReportController.php
@@ -62,9 +62,14 @@ class ReportController extends Controller
 
     public function statistics()
     {
-
+        // select count(*) as emps ,
+        // COUNT(CASE WHEN genderid = '1'  THEN 1 END) as males ,
+        // COUNT(CASE WHEN genderid = '2'  THEN 1 END) as females
+        //  from v_all_user_emp_info
         $result = Employee::select(
-            DB::raw('SELECT count(*) AS emps')
+            DB::raw("count(*) as emps"),
+            DB::raw("COUNT(CASE WHEN genderid = '1'  THEN 1 END) as males"),
+            DB::raw("COUNT(CASE WHEN genderid = '2'  THEN 1 END) as females")
         )->first();
 
         return successData($result);
