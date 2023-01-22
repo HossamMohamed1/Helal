fff0922d2dd16ab955a1f66aa5a3e3ee9430d2b3
Eslam ISmail <eslan.ismail93@gmail.com>
2023-01-22 10:44:12 +0200

    FA

--- backend/resources/lang/ar/dashboard.php
+++ backend/resources/lang/ar/dashboard.php
@@ -3,4 +3,8 @@
 return [
     'something_error' => 'خطأ غير معروف',
     'count' => 'العدد',
+    'attendance' => 'الحضور',
+    'late' => 'التاخير',
+    'no_late' => 'بدون تاخير',
+    'early' => 'الحضور باكرا',
 ];
--- backend/resources/lang/en/dashboard.php
+++ backend/resources/lang/en/dashboard.php
@@ -3,4 +3,8 @@
 return [
     'something_error' => 'Something Error',
     'count' => 'العدد',
+    'attendance' => 'الحضور',
+    'late' => 'التاخير',
+    'no_late' => 'بدون تاخير',
+    'early' => 'الحضور باكرا',
 ];
