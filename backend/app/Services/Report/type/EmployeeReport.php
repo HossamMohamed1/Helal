7eb6f281f3998ab296e49f5da64fdb3fd98955e4
Wakeb <Wakeb@Alharameen.gov.sa>
2023-01-26 16:31:21 +0300

    F

--- backend/app/Services/Report/type/EmployeeReport.php
+++ backend/app/Services/Report/type/EmployeeReport.php
@@ -398,12 +398,14 @@ class EmployeeReport extends BaseReport
     public function employeeExperienceQuery()
     {
         // select trunc(months_between(to_char(end_date), to_char(start_date)) /12) as experience  , count(emp_no) as count from v_all_user_emp_info group by trunc(months_between(to_char(end_date), to_char(start_date)) /12);
-        return dd(DB::connection('oracle')->table($this->mainTable)
+        return (DB::connection('oracle')->table($this->mainTable)
                 ->select(
                     DB::raw("trunc(months_between(to_char(end_date), to_char(start_date)) /12) as experience"),
-                    DB::raw("count(emp_no) as {$this->filter['columns'][0]}")
+                    // DB::raw("count(emp_no) as {$this->filter['columns'][0]}"),
+                    'emp_no'
                 )
-                ->groupBy('trunc(months_between(to_char(end_date), to_char(start_date)) /12)')
-                ->get());
+                // ->groupBy('experience')
+                ->get()
+            ->groupBy('experience'));
     }
 }
